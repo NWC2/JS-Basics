3fd89d9038bfec030417e629dae86a02d82f130e
AghaDev <mohamadagha2@gmail.com>
2023-08-30 15:55:10 +0300

    Step-3

--- javascript-basics-01/step_2.js
+++ javascript-basics-01/step_2.js
@@ -1,4 +1,4 @@
 let name = "Moe";
 let surname = "Agha";
 let city = "Beirut";
-alert("Name: "+name+"\nSurname: "+surname+"\nCity: "+city)
+alert("Name: "+name+"\nSurname: "+surname+"\nCity: "+city);
