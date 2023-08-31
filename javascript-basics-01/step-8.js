7f717e9a191c76ec86cf201d97f8f5f18029863f
AghaDev <mohamadagha2@gmail.com>
2023-08-31 09:27:50 +0300

    Step-8

---
+++ javascript-basics-01/step-8.js
@@ -0,0 +1,9 @@
+function CalculateAge(){
+    let age = parseFloat(document.getElementById("age").value)
+    if(age>18){
+        alert("User is over 18")
+    }
+    else
+        alert("User is under 18")
+    
+    }
