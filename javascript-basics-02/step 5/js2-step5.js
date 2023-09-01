39bcfbf285e311154983f34c428d580da6de27e8
AghaDev <mohamadagha2@gmail.com>
2023-09-01 14:40:16 +0300

    js2-step5

---
+++ javascript-basics-02/step 5/js2-step5.js
@@ -0,0 +1,13 @@
+var img1 = document.getElementById("image1");
+var img2 = document.getElementById("image2");
+var img3 = document.getElementById("image3");
+var img4 = document.getElementById("image4");
+var img5 = document.getElementById("image5");
+
+function agha(){
+img1.src = 'images/image1_2.jpg';
+img2.src = 'images/image2_2.jpg';
+img3.src = 'images/image3_2.jpg';
+img4.src = 'images/image4_2.jpg';
+img5.src = 'images/image5_2.jpg'
+}
